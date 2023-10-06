85983abff1d59ac7a55c993dee16125935fe8f6b
MaykonOliveiraC <maykonoliveiradev@gmail.com>
2023-10-05 23:41:29 -0300

    .

---
+++ exercicio/enumerate.py
@@ -0,0 +1,12 @@
+vendedores = ["Marcus", "Amanda", "Ale", "Carol"]
+vendas = [15, 20, 10, 30]
+
+for vendedor in vendedores:
+    print(vendedor)
+
+tamanho_lista = len(vendedores)
+
+for i, vendedor in enumerate(vendedores):
+    print(vendedor)
+    print(i)
+    
